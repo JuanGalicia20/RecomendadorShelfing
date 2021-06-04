61ff7e68ff88f92623218b045bfbbcd67076723c
elisamayoa <63657896+elisamayoa@users.noreply.github.com>
2021-06-03 18:51:47 -0600

    Update myBooksServlet.java

--- src/main/java/myBooksServlet.java
+++ src/main/java/myBooksServlet.java
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-=======
 package main.java;
-
->>>>>>> Stashed changes
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,25 +16,16 @@ import org.json.simple.JSONObject;
 import dataAccessLayer.EmbeddedNeo4j;
 
 /**
- * Servlet implementation class RegisterServlet
+ * Servlet implementation class LoginServlet
  */
-<<<<<<< Updated upstream
-@WebServlet("/RecommendationServlet")
-public class RegisterServlet extends HttpServlet {
-=======
 @WebServlet("/myBooksServlet")
 public class myBooksServlet extends HttpServlet {
->>>>>>> Stashed changes
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-<<<<<<< Updated upstream
-    public RecommendationServlet() {
-=======
     public myBooksServlet() {
->>>>>>> Stashed changes
         super();
         // TODO Auto-generated constructor stub
     }
@@ -46,43 +33,31 @@ public class myBooksServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		PrintWriter out = response.getWriter();
 	 	response.setContentType("application/json");
 	 	response.setCharacterEncoding("UTF-8");
+	 	JSONObject myResponse = new JSONObject();
+
+		JSonArray misLibros = new JSONArray();
+	 	
 	 	
 	 	String user = request.getParameter("user");
-	 	String name = request.getParameter("user_name");
-	 	String email = request.getParameter("user_email");
-	 	String pass = request.getParameter("user_pass");
-	 	
-	 	 try ( EmbeddedNeo4j greeter = new EmbeddedNeo4j( "bolt://localhost:7687", "neo4j", "uvgproy123@" ) )
+	 	try ( EmbeddedNeo4j greeter = new EmbeddedNeo4j( "bolt://localhost:7687", "neo4j", "uvgproy123@" ) )
 	        {
 	 		 	//System.out.println("Hola workkkkk");
-<<<<<<< Updated upstream
-			 	greeter.createUser(user, name, email, pass);
-			 	//System.out.println("Si workkkkk");
-=======
-			 	LinkedList<String> myBooksU = greeter.myBooksUser(user);
-			 	//System.out.println("Si workkkkk");
+	 		LinkedList<String> myBooksU = greeter.myBooksUser(user);
+	 		for ( int i = 0; i< myBooksU.size(); i++){
+				 misLibros.add(myBooksU.get(i));
 
-                for (int i = 0; i < myBooksU.size(); i++) {
-                    out.println( "<p>" + myBooksU.get(i) + "</a>" );
-                }
-              
-          } catch (Exception e) {
-              // TODO Auto-generated catch block
-              e.printStackTrace();
-          }
->>>>>>> Stashed changes
-	        	
-	        } catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-	 	
-	 	
+		}catch ( Exception e){
+			e.printStackTrace();
+		}
+	 	 
+	 	myResponse.put("libros", misLibros);
+	 	out.println(myResponse);
 	 	out.flush();  
 	 	
 	}
